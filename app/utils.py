eafefdb76a9bcc84a3e6328177ece7b535b71f83
Farhan Shahriar <fshahriar001@gmail.com>
2026-02-16 19:28:36 -0500

    cleaning rag.py by moving helpers

---
+++ app/utils.py
@@ -0,0 +1,12 @@
+"""
+utils.py
+
+Small reusable helpers:
+- citation parsing
+- text cleanup for PDF-ish chunks
+"""
+
+from __future__ import annotations
+
+import re
+from typing import Dict, List
